08f7be23350aa41345f9d759c7520c1603d00138
Tori Hedden <tori.hedden@gmail.com>
2016-02-17 20:29:11 -0500

    Adds beginnings of cat generator

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
       $($(this)).toggleClass("expand-view");
   });
 
+  $(".cat-button").click(function(){
+    // if ($("input").val() === 0){
+    //   $(".header")
+    // }
+
+    var catNumber = $("input").val();
+
+    if (catNumber == NaN){
+      console.log("That's not a number!")
+    } else {
+      console.log(parseInt(catNumber));
+    }
+
+  });
+
 });
