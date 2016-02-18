b25c2fa9b709352ac007ec22a60a167aaa700f30
Tori Hedden <tori.hedden@gmail.com>
2016-02-17 22:43:35 -0500

    Fixes indents for clarity

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -2,32 +2,32 @@ $(document).ready(function(){
   'use strict'
 
   $(".cat-button").click(function(){
-
-    if (parseInt($(".cat-number").val()) === 0){
+    
+    if (parseInt($(".cat-number").val()) < 1){
       $(".header").replaceWith("<div class = 'header'>you need cats</div>")
     } else if (parseInt($(".cat-number").val()) > 24){
       $(".header").replaceWith("<div class = 'header'>too many cats</div>")
     } else {
 
-    $(".cat-prompt").toggle();
+      $(".cat-prompt").toggle();
 
-    var newCat = '<li class = "cat-card">' +
+      var newCat = '<li class = "cat-card">' +
              '<img src="assets/images/cat.jpg">' +
              '<div class = "around-name">' +
                '<h3>Felis catus</h3>' +
              '</div>' +
            '</li>';
 
-    for (var i = 1; i < parseInt($(".cat-number").val()); i++){
-      $("body").append(newCat);
-        }
-      }
-    });
+      for (var i = 1; i < parseInt($(".cat-number").val()); i++){
+        $("body").append(newCat);
+          }
+    }
+  });
   //closes cat-button .click
 
-    $("li").click(function(){
-      $($(this)).toggleClass("expand-view");
-    });
+  $("li").click(function(){
+    $($(this)).toggleClass("expand-view");
+  });
   //closes li.click
 
 });//closes document.ready
