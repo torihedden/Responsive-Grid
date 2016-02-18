0ce0ec1d6a3342bae10ceaf4a4b82980d431cd5b
Tori Hedden <tori.hedden@gmail.com>
2016-02-17 21:57:44 -0500

    ADDSS WORKING CAT GENERATOR

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,23 +1,32 @@
 $(document).ready(function(){
   'use strict'
 
+  $(".cat-button").click(function(){
+    // console.log(parseInt($(".cat-number").val()));
+    $(".cat-prompt").toggle();
+
+    var newCat = '<li class = "cat-card">' +
+             '<img src="assets/images/cat.jpg">' +
+             '<div class = "around-name">' +
+               '<h3>Felis catus</h3>' +
+             '</div>' +
+           '</li>';
+
+    for (var i = 1; i < parseInt($(".cat-number").val()); i++){
+      $("body").append(newCat);
+        }
+  });
+  //closes cat-button .click
+
   $("li").click(function(){
       $($(this)).toggleClass("expand-view");
   });
+  //closes li.click
 
-  $(".cat-button").click(function(){
-    // if ($("input").val() === 0){
-    //   $(".header")
+});//closes document.ready
+
+    // if (catNumber == NaN){
+    //   console.log("That's not a number!")
+    // } else {
+    //   console.log(parseInt(catNumber, 10));
     // }
-
-    var catNumber = $("input").val();
-
-    if (catNumber == NaN){
-      console.log("That's not a number!")
-    } else {
-      console.log(parseInt(catNumber));
-    }
-
-  });
-
-});
