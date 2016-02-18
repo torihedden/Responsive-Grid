a61f3dcbcf827a53a1574d2d4692b65e360bcbbd
Tori Hedden <tori.hedden@gmail.com>
2016-02-17 23:12:26 -0500

    Adds comments to fix error tomorrow

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   'use strict'
 
   $(".cat-button").click(function(){
-    
+
     if (parseInt($(".cat-number").val()) < 1){
       $(".header").replaceWith("<div class = 'header'>you need cats</div>")
     } else if (parseInt($(".cat-number").val()) > 24){
@@ -24,6 +24,10 @@ $(document).ready(function(){
     }
   });
   //closes cat-button .click
+
+//$("body").on("click", "li", function)
+//only the first one expands, this is the same problem as in the text box homework from the jQuery widgets assignment. This event only works with the existing list element
+//$("#to-do-list").on('change', 'input', function() <-- use this format tomorrow morning to see if that fixes it
 
   $("li").click(function(){
     $($(this)).toggleClass("expand-view");
